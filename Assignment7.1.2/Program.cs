38bbad02a5b957e3eb543271b0016af49f76248e
connor gammill <connor.gammill@outlook.com>
2024-12-17 01:10:00 -0500

    Add project files.

---
+++ Assignment7.1.2/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Assignment7._1._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+			string word1 = "abc";
+			string word2 = "pqr";
+
+			string mergedWord = MergeStrings(word1, word2);
+			Console.WriteLine($"The merged string is: {mergedWord}");
+		}
+
+		public static string MergeStrings(string word1, string word2)
+		{
+			int maxLength = 0;
+			if (word1.Length > word2.Length)
+			{
+				maxLength = word1.Length;
+			}
+			else
+			{
+				maxLength = word2.Length;
+			}
+			char[] result = new char[word1.Length + word2.Length];
+			int index = 0;
+
+			for (int i = 0; i < maxLength; i++)
+			{
+				if (i < word1.Length)
+				{
+					result[index++] = word1[i];
+				}
+
+				if (i < word2.Length)
+				{
+					result[index++] = word2[i];
+				}
+			}
+
+			return new string(result);
+		}
+	}
+}
